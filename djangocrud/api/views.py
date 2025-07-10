7f080b81cf697590457b28ec79c595f6f95c730c
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-07-09 21:12:55 -0500

    configurando las rutas y agregando una vista

--- djangocrud/api/views.py
+++ djangocrud/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User,Group
+from rest_framework import viewsets
+from .models import Movie
+from .serializer import MovieSerializer
 
 # Create your views here.
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
