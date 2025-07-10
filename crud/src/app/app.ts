f3ebf56688f3155730116b166f6a35fca15843fd
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-07-09 21:58:56 -0500

    creando una vista para el html, tambien modificando el servicio y importando lo necesario en main.ts

--- crud/src/app/app.ts
+++ crud/src/app/app.ts
@@ -1,12 +1,32 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Api } from './api';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('crud');
+  movies = [
+    { id: 1, title: 'peli1', desc: 'Una peli muy buena', year: 2021 },
+    { id: 2, title: 'peli2', desc: 'Otra peli interesante', year: 2022 }
+  ];
+  constructor(private api:Api) {
+    this.getMovies();
+  }
+  getMovies = () => {
+    this.api.getAllMovies().subscribe(
+      data => {
+        console.log(data);
+        // this.movies = data; // Descomenta si data contiene un array válido
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  }
 }
